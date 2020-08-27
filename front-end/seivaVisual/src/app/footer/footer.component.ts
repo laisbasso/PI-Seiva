375844ddcba4b444bdd64360dc0d98c5821c1513
Victorhup <victorhup95@hotmail.com>
2020-08-27 14:29:47 -0300

    Footer

--- front-end/seivaVisual/src/app/footer/footer.component.ts
+++ front-end/seivaVisual/src/app/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  faFacebook = faFacebook
+  faInstagram = faInstagram
+  faLinkedin = faLinkedin
   constructor() { }
+  
 
   ngOnInit(): void {
   }
