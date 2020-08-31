37030ffb41635ec0f5094adf89322fe73df9cb56
Lais <laisbasso@live.com>
2020-08-31 13:49:01 -0300

    Inserindo rotas e atualizando carrossel

--- front-end/seivaVisual/src/app/app-routing.module.ts
+++ front-end/seivaVisual/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeInicioComponent } from './home-inicio/home-inicio.component';
+import { EquipeComponent } from './equipe/equipe.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home-inicio', pathMatch: 'full' },
+	{ path: 'home-inicio', component: HomeInicioComponent },
+  { path: 'equipe', component: EquipeComponent },
+  { path: 'sobre', component: SobreComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
