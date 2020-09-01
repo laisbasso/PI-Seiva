0ba92b766694fd007f0de0d72491f10702f0f42a
matheuxmuller <matheuxmuller@gmail.com>
2020-09-01 15:54:10 -0300

    Inclusão do feed e método GET

--- front-end/seivaVisual/src/app/app-routing.module.ts
+++ front-end/seivaVisual/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeInicioComponent } from './home-inicio/home-inicio.component';
 import { EquipeComponent } from './equipe/equipe.component';
 import { SobreComponent } from './sobre/sobre.component';
+import { FeedComponent } from './feed/feed.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home-inicio', pathMatch: 'full' },
 	{ path: 'home-inicio', component: HomeInicioComponent },
   { path: 'equipe', component: EquipeComponent },
-  { path: 'sobre', component: SobreComponent }
+  { path: 'sobre', component: SobreComponent },
+  { path: 'feed', component: FeedComponent }
 ];
 
 @NgModule({
--- front-end/seivaVisual/src/app/service/tema.service.ts
+++ front-end/seivaVisual/src/app/service/tema.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TemaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', localStorage.getItem('token'))
+  }
+
+  getAllTemas() {
+    return this.http.get('http://localhost:9000/tema', this.token)
+  }
+
+  getByIdTema(id: number) {
+    return this.http.get(`http://localhost:9000/tema/${id}`, this.token)
+  }
 }
