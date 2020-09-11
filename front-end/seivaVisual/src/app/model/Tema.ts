a8f142b3b0fddc2b633418baadcf83b5af962d6c
Lais <laisbasso@live.com>
2020-09-11 11:05:36 -0300

    Adicionando imagens aos cards de temas

--- front-end/seivaVisual/src/app/model/Tema.ts
+++ front-end/seivaVisual/src/app/model/Tema.ts
@@ -3,5 +3,6 @@ import { Postagem } from './Postagem';
 export class Tema {
     public id: number;
     public descricao: string;
+    public imagem: string;
     public postagem: Postagem []
 }
--- front-end/seivaVisual/src/app/service/tema.service.ts
+++ front-end/seivaVisual/src/app/service/tema.service.ts
@@ -14,23 +14,23 @@ export class TemaService {
   }
 
   getAllTemas() {
-    return this.http.get('http://localhost:9000/tema', this.token)
+    return this.http.get('http://localhost:8080/tema', this.token)
   }
 
   getByIdTema(id: number) {
-    return this.http.get(`http://localhost:9000/tema/${id}`, this.token)
+    return this.http.get(`http://localhost:8080/tema/${id}`, this.token)
   }
 
   postTema(tema: Tema) {
-    return this.http.post('http://localhost:9000/tema', tema, this.token)
+    return this.http.post('http://localhost:8080/tema', tema, this.token)
   }
 
   putTema(tema: Tema) {
-    return this.http.put('http://localhost:9000/tema', tema, this.token)
+    return this.http.put('http://localhost:8080/tema', tema, this.token)
   }
 
   deleteTema(id: number){
-    return this.http.delete(`http://localhost:9000/tema/${id}`, this.token)
+    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
   }
   
 }
